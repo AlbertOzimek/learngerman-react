1b66163f0183b88482822dffe2b333ff0ceb7871
Albert Ozimek <albert.ozimek@gmail.com>
2019-05-15 12:32:34 +0200

    ng-if

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,22 @@
 import React, { useState } from "react";
 
-function getBadgeClasses(state) {
-  let classes = "badge m-2 badge-";
-  classes += state.count === 0 ? "warning" : "primary";
-  return classes;
-}
-
 const Counter = props => {
   const [state] = useState({
     count: 0,
     tags: ["tag1", "tag2", "tag3"]
   });
-  const formatCount = () => {
-    return state.count === 0 ? "Zero" : state.count;
-  };
-  return (
-    <React.Fragment>
-      <span className={getBadgeClasses(state)}>
-        Count: {formatCount(state.count)}
-      </span>
-      <button className="btn btn-secondary btn-sm">Increment</button>
+
+  const renderTags = () => {
+    if (state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
       <ul>
         {state.tags.map(tag => (
           <li key={tag}>{tag}</li>
         ))}
       </ul>
-    </React.Fragment>
-  );
+    );
+  };
+  return <React.Fragment>{renderTags()}</React.Fragment>;
 };
 
 export default Counter;
