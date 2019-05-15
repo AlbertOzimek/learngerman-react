e806a8134effa802b4f5d6153c081a78fd400a54
Albert Ozimek <albert.ozimek@gmail.com>
2019-05-15 12:18:16 +0200

    equivalent of ng loop - map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,7 +8,8 @@ function getBadgeClasses(state) {
 
 const Counter = props => {
   const [state] = useState({
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   });
   const formatCount = () => {
     return state.count === 0 ? "Zero" : state.count;
@@ -19,6 +20,11 @@ const Counter = props => {
         Count: {formatCount(state.count)}
       </span>
       <button className="btn btn-secondary btn-sm">Increment</button>
+      <ul>
+        {state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 };
