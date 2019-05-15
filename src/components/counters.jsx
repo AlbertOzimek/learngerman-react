b34cda8db0e3c976d5300c3bc8e677fe56ed6cd5
Albert Ozimek <albert.ozimek@gmail.com>
2019-05-15 14:50:23 +0200

    adding counters component

---
+++ src/components/counters.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./counter";
+
+const Counters = () => {
+  return (
+    <React.Fragment>
+      <p>countersXXX</p>
+      <Counter />
+    </React.Fragment>
+  );
+};
+
+export default Counters;
