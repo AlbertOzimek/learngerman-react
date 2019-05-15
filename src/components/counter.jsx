6ec5a8a0f561982a1f1dd792177d81ef42c94314
Albert Ozimek <albert.ozimek@gmail.com>
2019-05-15 15:26:44 +0200

    counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,46 +2,36 @@ import React, { useState } from "react";
 
 function getBadgeClasses(state) {
   let classes = "badge m-2 badge-";
-  classes += state.count === 0 ? "warning" : "primary";
+  classes += state.value === 0 ? "warning" : "primary";
   return classes;
 }
 
 const Counter = props => {
   const [state, setState] = useState({
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: props.value
   });
 
   const handleIncrement = product => {
     console.log(product);
-    setState({ ...state, count: state.count + 1 });
+    setState({ ...state, value: state.value + 1 });
   };
 
   const formatCount = () => {
-    return state.count === 0 ? "Zero" : state.count;
-  };
-
-  const renderTags = () => {
-    if (state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <ul>
-        {state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+    return state.value === 0 ? "Zero" : state.value;
   };
 
   const product = { id: 123 };
   return (
     <React.Fragment>
       <span className={getBadgeClasses(state)}>
-        Count: {formatCount(state.count)}
+        Count: {formatCount(state.value)}
       </span>
-
-      {state.tags.length === 0 && "Please create a new tag!"}
-      {renderTags()}
-      <button onClick={() => handleIncrement(product)}>Increment</button>
+      <button
+        className="btn btn-secondary"
+        onClick={() => handleIncrement(product)}
+      >
+        Increment
+      </button>
     </React.Fragment>
   );
 };
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./counter";
 
 const Counters = () => {
+  const [state] = useState({
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 2 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  });
   return (
     <React.Fragment>
-      <p>countersXXX</p>
-      <Counter />
+      {state.counters.map(counter => (
+        <React.Fragment>
+          <Counter key={counter.id} value={counter.value} />
+          <br />
+        </React.Fragment>
+      ))}
     </React.Fragment>
   );
 };
